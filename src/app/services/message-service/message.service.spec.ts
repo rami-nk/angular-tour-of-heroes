6ebd409cc2825ae0210ca01d83ef3f1e6ec4cab3
Rami Nakkar <rami.nakkar@xitaso.com>
2021-09-14 08:42:19 +0200

    added tests for MessageService

--- src/app/services/message-service/message.service.spec.ts
+++ src/app/services/message-service/message.service.spec.ts
@@ -6,11 +6,32 @@ describe('MessageService', () => {
   let service: MessageService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [MessageService]
+    });
     service = TestBed.inject(MessageService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('messages array should be empty on init', () => {
+    expect(service.messages.length).toBe(0);
+  });
+
+  it('messages array should contain one message with the content "message1"', () => {
+    service.add("message 1");
+    expect(service.messages.length).toBe(1);
+    expect(service.messages[0]).toContain("message 1");
+  })
+
+  it('messages array should contain 100 elements after calling #add and it should be empty after calling #clear', () => {
+    for (let i=0; i < 100; i++) {
+      service.add("message " + i);
+    }
+    expect(service.messages.length).toBe(100);
+    service.clear();
+    expect(service.messages.length).toBe(0);
   });
 });
